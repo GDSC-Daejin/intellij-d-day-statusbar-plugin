3e3fab781c5ba8a105aff86e65e0be26a12fda81
Harry Park <jungsanmango@gmail.com>
2022-03-22 13:29:32 +0900

    d-day 표시 수정

--- src/main/kotlin/DdayStatusBar.kt
+++ src/main/kotlin/DdayStatusBar.kt
@@ -16,12 +16,13 @@ class DdayStatusBarWidgetPresentation(private val statusBar: StatusBar) : Status
 
     private val date = Instant.parse("1997-09-14T00:00:00Z")
 
-    private fun calculateDuration(): Duration {
-        return Clock.System.now() - date
-    }
-
     override fun getText(): String {
-        return "D-${calculateDuration().inWholeDays + 2}"
+        val duration = (Clock.System.now() - date).inWholeDays + 2
+        return when {
+            duration == 0L -> "D-day"
+            duration > 0L -> "D+$duration"
+            else -> "D-${-duration}"
+        }
     }
 
     override fun getAlignment() = Component.CENTER_ALIGNMENT
